58c44cbfbf2869afb739114f7829161ce246b3ce
Duane <kingdallred@outlook.com>
2025-04-09 21:18:00 -0600

    Moved some code around and added logo animation

---
+++ javascript/limitlessSite.js
@@ -0,0 +1,8 @@
+document.addEventListener("DOMContentLoaded", function() {
+
+    const limitlessLinks = document.getElementById("limitlessLinks");
+
+    limitlessLinks.addEventListener("click", function() {
+        
+    });
+});
